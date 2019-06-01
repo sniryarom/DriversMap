09e26e912b24b454d5ced2c8b4e06d23ac137d3d
snirya <snir@homex.co.il>
2019-06-01 23:57:23 +0700

    completed adding realtime stream

--- client/src/App.js
+++ client/src/App.js
@@ -18,6 +18,14 @@ const imgStyle = {
   height: '100%'
 }
 
+class DriverData {
+
+  constructor(id, state, position) {
+    this.id = id;
+    this.state = state;
+    this.position = position;
+  }
+}
 
 const Marker = ({ title }) => (
   <div style={markerStyle}>
@@ -31,7 +39,7 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      center: {lat: 5.6219868, lng: -0.23223},
+      center: {lat: 48.83921794298303, lng: 2.3558752615457},
       locations: [],
       users_online: [],
       current_user: '',
@@ -39,57 +47,88 @@ class App extends Component {
     }
   }
 
-  callAPI() {
-    fetch("http://localhost:9000/drivers")
-      .then(res => res.json())
-      .then(json => { 
-        this.setState({ 
-          center: json.center, 
-          locations: json.locations
-        });
-        console.log(json);
-      })
-      .catch(err => {
-        console.log('an error has occured while fetching data from API: ' + err);
-      });
+  // callAPI() {
+  //   fetch("http://localhost:9000/drivers")
+  //     .then(res => res.json())
+  //     .then(json => { 
+  //       this.setState({ 
+  //         center: json.center, 
+  //         locations: json.locations
+  //       });
+  //       console.log(json);
+  //     })
+  //     .catch(err => {
+  //       console.log('an error has occured while fetching data from API: ' + err);
+  //     });
 
-    }
+  //   }
 
 
-  componentDidMount() {
+  componentWillMount() {
     //this.callAPI();
-    subscribeToDrivers((err, timestamp) => this.setState({ timestamp }));
+    subscribeToDrivers((err, data) => {
+      let dataObj = JSON.parse(data);
+        //console.log('updated driver data (from server): ' + JSON.stringify(dataObj));
+        let driverData = new DriverData(dataObj.id, dataObj.state, dataObj.position);
+        var newLocations = this.state.locations.slice() //copy the array
+        let driverFound = false;
+        for (var i = 0; i < newLocations.length - 1; i++) {
+          if (newLocations[i].id === driverData.id) {
+            newLocations[i] = driverData; // update the specific driver location
+            driverFound = true;
+            break;
+          }
+        }
+        if (!driverFound && newLocations.length < 10) {
+          newLocations.push(driverData);
+        }
+        //console.log('current: ' + this.state.locations);
+        //console.log('updated: ' + newLocations);
+        this.setState({locations: newLocations}); //set the new state
+        
+    //   this.setState( state => {
+    //     var locations = state.locations.map(data, index => {
+    //       if (index === driverData.id) {
+    //         return driverData;
+    //       } else {
+    //         return data;
+    //       }
+    //     });
+    //     return { locations };
+    //    });
+     });
   }
 
    render() {
     let locationMarkers = this.state.locations.map((value, index) => {
+      //console.log('rendering driver position lat: ' + value.position[0] + ', lng: ' + value.position[1])
     return (
         <Marker
           key={index}
-          title={value.username}
-          lat={value.lat}
-          lng={value.lng}
+          title={value.state}
+          lat={value.position[0]}
+          lng={value.position[1]}
         >
         </Marker>
       );
     });
 
     return (
-      // <div >
-      //   <GoogleMap
-      //     style={mapStyles}
-      //     bootstrapURLKeys={{ key: 'AIzaSyBSsICbZ6zyliCdgYcUyEIW5VVq1tIyshI' }}
-      //     center={this.state.center}
-      //     zoom={17}
-      //   >
-      //     {locationMarkers}
-      //   </GoogleMap>
-      // </div>
-      <div className="App">
-      <p className="App-intro">
-      This is the timer value: {this.state.timestamp}
-      </p>
-    </div>
+      <div >
+        <GoogleMap
+          style={mapStyles}
+          bootstrapURLKeys={{ key: 'AIzaSyBSsICbZ6zyliCdgYcUyEIW5VVq1tIyshI' }}
+          center={this.state.center}
+          zoom={14}
+        >
+          {locationMarkers}
+        </GoogleMap>
+      </div>
+    //   <div className="App">
+    //   <p className="App-intro">
+    //   This is the timer value: {this.state.timestamp}
+    //   </p>
+    // </div>
 
     )
   }
--- client/src/api.js
+++ client/src/api.js
@@ -1,7 +1,7 @@
 import openSocket from 'socket.io-client';
 
 const socket = openSocket('http://localhost:9000');
-const interval = 1000;
+const interval = 100;
 
 function subscribeToDrivers(cb) {
     socket.on('drivers', timestamp => cb(null, timestamp));
