1c72fcd80e1df37044fc482772f1a9584d7caea5
snirya <snir@homex.co.il>
2019-06-02 22:37:49 +0700

    optional goal initial implementation done

--- client/src/App.js
+++ client/src/App.js
@@ -2,37 +2,46 @@ import React, { Component } from 'react';
 import GoogleMap from 'google-map-react';
 import './App.css';
 import { subscribeToDrivers } from './api';
+import DriverData from './DriverData';
 
 const mapStyles = {
   width: '100%',
-  height: '100%'
+  height: '95%'
 }
 
 const markerStyle = {
   height: '50px',
   width: '50px',
-  marginTop: '-50px'
+  marginTop: '-100px',
+  color: 'green',
+  textAlign: 'center'
 }
 
 const imgStyle = {
   height: '100%'
 }
 
-class DriverData {
-
-  constructor(id, state, position) {
-    this.id = id;
-    this.state = state;
-    this.position = position;
+const Marker = ({ title, status }) => {
+  var cssClass = '';
+  switch(status) {
+    case 'available':
+      cssClass = 'marker-title available';
+      break;
+    case 'ride':
+      cssClass = 'marker-title ride';
+      break;
+    case 'unavailable':
+    default:
+      cssClass = 'marker-title unavailable';
+      break;
   }
-}
-
-const Marker = ({ title }) => (
+  return (
   <div style={markerStyle}>
     <img style={imgStyle} src="https://cdn2.iconfinder.com/data/icons/vehicle-type/1024/suv-512.png" alt={title} />
-    <h3>{title}</h3>
+    <h3 class={cssClass}>{title}</h3>
   </div>
-);
+  )
+};
 
 class App extends Component {
 
@@ -40,29 +49,9 @@ class App extends Component {
     super(props)
     this.state = {
       center: {lat: 48.83921794298303, lng: 2.3558752615457},
-      locations: [],
-      users_online: [],
-      current_user: '',
-      timestamp: 'no stamps yet'
+      locations: []
     }
   }
-
-  // callAPI() {
-  //   fetch("http://localhost:9000/drivers")
-  //     .then(res => res.json())
-  //     .then(json => { 
-  //       this.setState({ 
-  //         center: json.center, 
-  //         locations: json.locations
-  //       });
-  //       console.log(json);
-  //     })
-  //     .catch(err => {
-  //       console.log('an error has occured while fetching data from API: ' + err);
-  //     });
-
-  //   }
-
 
   componentWillMount() {
     //this.callAPI();
@@ -85,27 +74,18 @@ class App extends Component {
         //console.log('current: ' + this.state.locations);
         //console.log('updated: ' + newLocations);
         this.setState({locations: newLocations}); //set the new state
-        
-    //   this.setState( state => {
-    //     var locations = state.locations.map(data, index => {
-    //       if (index === driverData.id) {
-    //         return driverData;
-    //       } else {
-    //         return data;
-    //       }
-    //     });
-    //     return { locations };
-    //    });
      });
   }
 
    render() {
     let locationMarkers = this.state.locations.map((value, index) => {
+      var status = Date.now() - value.updateTime > 10000 ? 'unavailable' : value.state;
       //console.log('rendering driver position lat: ' + value.position[0] + ', lng: ' + value.position[1])
     return (
         <Marker
           key={index}
-          title={value.state}
+          title={status === 'unavailable' ? 'no service' : value.state}
+          status={status}
           lat={value.position[0]}
           lng={value.position[1]}
         >
@@ -114,20 +94,22 @@ class App extends Component {
     });
 
     return (
-      <div class="title">
-         <h2 class="title">hi this is Kapten drivers location app</h2>
+      <div>
+        <div class="title">
+          <h2 class="title"></h2>
+        </div>
+        <div class="mapContainer" >
+          <GoogleMap
+            style={mapStyles}
+            bootstrapURLKeys={{ key: 'AIzaSyBSsICbZ6zyliCdgYcUyEIW5VVq1tIyshI' }}
+            center={this.state.center}
+            zoom={14}
+          >
+          
+            {locationMarkers}
+          </GoogleMap>
       </div>
-      // <div >
-      //   <GoogleMap
-      //     style={mapStyles}
-      //     bootstrapURLKeys={{ key: 'AIzaSyBSsICbZ6zyliCdgYcUyEIW5VVq1tIyshI' }}
-      //     center={this.state.center}
-      //     zoom={14}
-      //   >
-        
-      //     {locationMarkers}
-      //   </GoogleMap>
-      // </div>
+     </div>
     //   <div className="App">
     //   <p className="App-intro">
     //   This is the timer value: {this.state.timestamp}
---
+++ client/src/DriverData.js
@@ -0,0 +1,13 @@
+'use strict';
+
+class DriverData {
+
+    constructor(id, state, position) {
+      this.id = id;
+      this.state = state;
+      this.position = position;
+      this.updateTime = Date.now();
+    }
+  }
+
+  export default DriverData;
